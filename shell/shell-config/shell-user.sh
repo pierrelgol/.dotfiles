1840a84e0eabda6660972bb09eb38890fdfc38ef
PierreLgol <plgol.perso@gmail.com>
2024-05-17 17:41:53 +0200

    upt

--- shell/shell-config/shell-user.sh
+++ shell/shell-config/shell-user.sh
@@ -82,3 +82,12 @@ fi
 export XDG_DATA_DIRS="${XDG_DATA_DIRS:-/usr/local/share/:/usr/share/}"
 export XDG_CONFIG_DIRS="${XDG_CONFIG_DIRS:-/etc/xdg}"
 export XDG_DATA_HOME XDG_CONFIG_HOME XDG_STATE_HOME XDG_CACHE_HOME XDG_RUNTIME_DIR XDG_DATA_DIRS XDG_CONFIG_DIRS
+
+################################################################################
+# PATH                                                                         #
+################################################################################
+
+path+=('$HOME/local/bin')
+path+=('$HOME/local/repo')
+path+=('$HOME/.local/bin')
+path+=('$HOME/.local/share/applications')
